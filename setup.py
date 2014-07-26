395c16a777f1eb1135114c62643d0606bed36388
July Tikhonov <july.tikh@gmail.com>
2014-07-26 23:48:03 +0400

    Change repository layout

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='jeolm',
     packages=['jeolm', 'jeolm.driver'],
-    package_dir={'jeolm' : 'python'},
+    package_dir={'jeolm' : 'source/jeolm'},
     package_data={'jeolm' : ['resources/*']}
 )
 
