e981db7c3a13af3e6f0a86f5768572277463d981
July Tikhonov <july.tikh@gmail.com>
2015-03-02 00:12:50 +0300

    Fix behavior of jeolm.commands.resolve_inpaths()
    Symbolic links in source tree are now allowed and are dealt with in the
    same way as if they were normal files/directories.
    Infinitely nested directory structure may cause unexpected behavior
    (probably will be fixed soon).

--- source/jeolm/commands.py
+++ source/jeolm/commands.py
@@ -147,12 +147,33 @@ def resolve_inpaths(paths, *, source_dir, viewpoint=None):
             if not path.is_absolute():
                 raise RuntimeError(path)
     for path in paths:
-        try:
-            path = path.resolve()
-        except FileNotFoundError:
-            pass
-            # TODO new version of resolve() (in 3.5) should solve this issue
+        path = _path_resolve_up_to(path, limit_path=source_dir)
         yield PurePosixPath(path).relative_to(source_dir)
+
+def _path_resolve_up_to(path, *, limit_path):
+    assert isinstance(path, Path), path
+    assert path.is_absolute(), path
+    assert isinstance(limit_path, Path), path
+    assert limit_path.is_absolute(), limit_path
+    resolved = Path('/')
+    parts = iter(path.parts[1:])
+    for part in parts:
+        resolved = (resolved / part).resolve()
+        if _path_is_subpath(limit_path, resolved):
+            break
+    return resolved.joinpath(*parts)
+
+def _path_is_subpath(a_path, b_path):
+    assert isinstance(a_path, PurePosixPath), a_path
+    assert a_path.is_absolute(), a_path
+    assert isinstance(b_path, PurePosixPath), b_path
+    assert b_path.is_absolute(), b_path
+    a_parts = a_path.parts
+    b_parts = b_path.parts
+    if not all(a == b for a, b in zip(a_parts, b_parts)):
+        raise ValueError("Paths '{}' and '{}' are uncomparable."
+            .format(a_path, b_path))
+    return len(a_parts) >= len(b_parts)
 
 @contextmanager
 def refrain_called_process_error():
