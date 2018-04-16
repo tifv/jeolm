5adadf5f07342b2f598d0ed757a03a3c7c6c0661
July Tikhonov <july.tikh@gmail.com>
2018-04-16 11:40:36 +0300

    minor fix

--- source/jeolm/driver/regular.py
+++ source/jeolm/driver/regular.py
@@ -1573,7 +1573,7 @@ class RegularDriver(MetaRecords): # {{{1
         try:
             return check_and_set(mapping, key, value)
         except ClashingValueError as error:
-            raise DriverError("unable to set different value") from error
+            raise DriverError(*error.args) from error
 
 # }}}1
 # vim: set foldmethod=marker :
