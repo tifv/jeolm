e67e2aaa432ec5b08d6afb93c10c98b844b0af98
July Tikhonov <july.tikh@gmail.com>
2019-06-11 20:07:22 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from distutils.core import setup
 setup(
     name='jeolm',
     packages=[
-        'jeolm', 'jeolm.driver', 'jeolm.node', 'jeolm.node_factory',
+        'jeolm',
+        'jeolm.utils', 'jeolm.driver', 'jeolm.node', 'jeolm.node_factory',
         'jeolm.commands', 'jeolm.scripts' ],
     package_dir={'jeolm' : 'source/jeolm'},
     package_data={'jeolm' : ['resources/*']}
