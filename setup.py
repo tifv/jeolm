d0c4c9519f6c7c0f40b74247fbfecb684f6cc132
July Tikhonov <july.tikh@gmail.com>
2023-05-31 08:58:30 +0600

    adapt setup.py to pip use

--- setup.py
+++ setup.py
@@ -7,6 +7,11 @@ setup(
         'jeolm.utils', 'jeolm.driver', 'jeolm.node', 'jeolm.node_factory',
         'jeolm.commands', 'jeolm.scripts' ],
     package_dir={'jeolm' : 'source/jeolm'},
-    package_data={'jeolm' : ['resources/*']}
+    package_data={'jeolm' : ['resources/*']},
+    install_requires=[
+        'typing_extensions',
+        'pyyaml',
+        'pyinotify',
+    ],
 )
 
